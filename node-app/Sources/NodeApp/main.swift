2948899a3eb6f9244ade5e9e2044ad4714e309c7
Ivan Oparin <oparin@me.com>
2026-07-05 06:23:43 +0400

    fix: Edit menu so Cmd+V/C/X/A work in the onboarding code field

--- node-app/Sources/NodeApp/main.swift
+++ node-app/Sources/NodeApp/main.swift
@@ -25,6 +25,18 @@ mainMenu.addItem(appMenuItem)
 let appMenu = NSMenu()
 appMenu.addItem(NSMenuItem(title: "Quit Pulsar", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
 appMenuItem.submenu = appMenu
+// Edit menu: without it Cmd+V/C/X/A do nothing in the onboarding code field.
+let editMenuItem = NSMenuItem()
+mainMenu.addItem(editMenuItem)
+let editMenu = NSMenu(title: "Edit")
+editMenu.addItem(withTitle: "Undo", action: Selector(("undo:")), keyEquivalent: "z")
+editMenu.addItem(withTitle: "Redo", action: Selector(("redo:")), keyEquivalent: "Z")
+editMenu.addItem(.separator())
+editMenu.addItem(withTitle: "Cut", action: #selector(NSText.cut(_:)), keyEquivalent: "x")
+editMenu.addItem(withTitle: "Copy", action: #selector(NSText.copy(_:)), keyEquivalent: "c")
+editMenu.addItem(withTitle: "Paste", action: #selector(NSText.paste(_:)), keyEquivalent: "v")
+editMenu.addItem(withTitle: "Select All", action: #selector(NSText.selectAll(_:)), keyEquivalent: "a")
+editMenuItem.submenu = editMenu
 app.mainMenu = mainMenu
 
 let defaultCoordinatorBase = "https://barycenter.relux.works"
